5cbfa4a1f12b6c79a996103134f374eeebd31ffa
Lewis Wright <lewis@vivait.co.uk>
2014-06-25 19:16:00 +0100

    Replaced the locator to disable tests on spec classes since it gets the line numbers wrong for them.

--- src/Vivait/ScrutinizerFormatterExtension/Extension.php
+++ src/Vivait/ScrutinizerFormatterExtension/Extension.php
@@ -5,6 +5,7 @@ namespace Vivait\ScrutinizerFormatterExtension;
 use PhpSpec\Extension\ExtensionInterface;
 use PhpSpec\Formatter\Presenter\StringPresenter;
 use PhpSpec\ServiceContainer;
+use Vivait\ScrutinizerFormatterExtension\Loader\ResourceLoader;
 
 class Extension implements ExtensionInterface
 {
@@ -13,19 +14,37 @@ class Extension implements ExtensionInterface
      */
     public function load(ServiceContainer $container)
     {
-        $this->addFormatter($container, 'scrutinizer', 'Vivait\ScrutinizerFormatterExtension\Formatter\ScrutinizerFormatter');
+        $this->addFormatter(
+          $container,
+          'scrutinizer',
+          'Vivait\ScrutinizerFormatterExtension\Formatter\ScrutinizerFormatter'
+        );
+
+        $this->overrideLoader($container);
+    }
+
+    protected function overrideLoader(ServiceContainer $container)
+    {
+        $container->setShared(
+          'loader.resource_loader',
+          function (ServiceContainer $c) {
+              return new Loader\ResourceLoader($c->get('locator.resource_manager'));
+          }
+        );
     }
 
     /**
      * Add a formatter to the service container
      *
      * @param ServiceContainer $container
-     * @param string           $name
-     * @param string           $class
+     * @param string $name
+     * @param string $class
      */
     protected function addFormatter(ServiceContainer $container, $name, $class)
     {
-        $container->set('formatter.formatters.' . $name, function (ServiceContainer $c) use ($class) {
+        $container->set(
+          'formatter.formatters.' . $name,
+          function (ServiceContainer $c) use ($class) {
               $c->set('formatter.presenter', new StringPresenter($c->get('formatter.presenter.differ')));
 
               /** @var ServiceContainer $c */
@@ -34,6 +53,7 @@ class Extension implements ExtensionInterface
                 $c->get('console.io'),
                 $c->get('event_dispatcher.listeners.stats')
               );
-          });
+          }
+        );
     }
 }
---
+++ src/Vivait/ScrutinizerFormatterExtension/Loader/ResourceLoader.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Vivait\ScrutinizerFormatterExtension\Loader;
+
+use PhpSpec\Loader\Suite;
+use PhpSpec\Locator\ResourceManager;
+
+class ResourceLoader extends \PhpSpec\Loader\ResourceLoader {
+
+    /**
+     * @var \PhpSpec\Locator\ResourceManager
+     */
+    private $manager;
+
+    /**
+     * @param ResourceManager $manager
+     */
+    public function __construct(ResourceManager $manager)
+    {
+        parent::__construct($manager);
+        $this->manager = $manager;
+    }
+
+    /**
+     * @param string       $locator
+     * @param integer|null $line
+     *
+     * @return Suite
+     */
+    public function load($locator, $line = null)
+    {
+        foreach ($this->manager->locateResources($locator) as $resource) {
+            if ($resource->getSpecFilename() == $locator){
+                return new Suite;
+            }
+        }
+
+        return parent::load($locator, $line);
+    }
+}
